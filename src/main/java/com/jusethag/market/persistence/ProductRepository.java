903503ca3d68d056b8f426442d860a3567855998
JusethAg <mauagueda@gmail.com>
2020-09-13 15:45:50 -0500

    Add Product service implementation

--- src/main/java/com/jusethag/market/persistence/ProductRepository.java
+++ src/main/java/com/jusethag/market/persistence/ProductRepository.java
@@ -3,6 +3,7 @@ package com.jusethag.market.persistence;
 import com.jusethag.market.domain.Product;
 import com.jusethag.market.persistence.crud.ProductCrudRepository;
 import com.jusethag.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,10 @@ import java.util.Optional;
 @Repository
 public class ProductRepository implements com.jusethag.market.domain.repository.ProductRepository {
 
+    @Autowired
     private ProductCrudRepository productCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
