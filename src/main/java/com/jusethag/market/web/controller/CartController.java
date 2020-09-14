9774b012d5fe069205ff4ce2da49ed9f12b36d8a
JusethAg <mauagueda@gmail.com>
2020-09-13 22:43:06 -0500

    Document API using Swagger utility and Swagger UI

--- src/main/java/com/jusethag/market/web/controller/CartController.java
+++ src/main/java/com/jusethag/market/web/controller/CartController.java
@@ -2,6 +2,10 @@ package com.jusethag.market.web.controller;
 
 import com.jusethag.market.domain.Cart;
 import com.jusethag.market.domain.service.CartService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +21,31 @@ public class CartController {
     private CartService cartService;
 
     @GetMapping
+    @ApiOperation("Get all carts on the market")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Cart>> getAll() {
         return new ResponseEntity<>(cartService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{clientId}")
-    public ResponseEntity<List<Cart>> getByClient(@PathVariable("clientId") String clientId) {
+    @ApiOperation("Search carts by client")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Carts not found")
+    })
+    public ResponseEntity<List<Cart>> getByClient(
+            @ApiParam(value = "Client ID", required = true, example = "4546221")
+            @PathVariable("clientId") String clientId) {
         return cartService.getByClient(clientId)
                 .map(carts -> new ResponseEntity<>(carts, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
+    @ApiOperation("Create a cart")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cart created")
+    })
     public ResponseEntity<Cart> save(@RequestBody Cart cart) {
         return new ResponseEntity<>(cartService.save(cart), HttpStatus.CREATED);
     }
--- src/main/java/com/jusethag/market/web/controller/ProductController.java
+++ src/main/java/com/jusethag/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.jusethag.market.web.controller;
 
 import com.jusethag.market.domain.Product;
 import com.jusethag.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,31 +22,61 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
+    @ApiOperation("Get all market products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable("productId") int productId) {
+    @ApiOperation("Search a product by ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(
+            @ApiParam(value = "Product ID", required = true, example = "2")
+            @PathVariable("productId") int productId) {
+
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+    @ApiOperation("Search products by category")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Products not found")
+    })
+    public ResponseEntity<List<Product>> getByCategory(
+            @ApiParam(value = "Category ID", required = true, example = "1")
+            @PathVariable("categoryId") int categoryId) {
         return productService.getByCategory(categoryId)
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product) {
+    @ApiOperation("Create a product")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Product created")
+    })
+    public ResponseEntity<Product> save(
+            @ApiParam(value = "Product object", required = true)
+            @RequestBody Product product) {
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{productId}")
-    public ResponseEntity delete(@PathVariable("productId") int productId) {
+    @ApiOperation("Delete a product")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
+    public ResponseEntity delete(
+            @ApiParam(value = "Product ID", required = true, example = "1")
+            @PathVariable("productId") int productId) {
         if (productService.delete(productId)) {
             return new ResponseEntity(HttpStatus.OK);
         } else {
